8975654627353f395dac34638b5af62a97a9eb13
Adrian Schmitz <adrian@root.life>
2023-02-01 22:06:39 -0600

    organizing flake outputs

--- flake.nix
+++ flake.nix
@@ -14,51 +14,49 @@
 
   outputs = { self, ... }@inputs: {
 
-    templates = {
-      golang-basic = {
-        path = ./golang-basic;
-        description = "Golang project.";
-      };
-      python-basic = {
-        path = ./python-basic;
-        description = "Python project.";
-      };
-      rust-aws-sam = {
-        path = ./rust-aws-sam;
-        description = "Rust in AWS Lambda with SAM.";
-      };
-      rust-basic = {
-        path = ./rust-basic;
-        description = "Rust project.";
-      };
-      rust-dioxus = {
-        path = ./rust-dioxus;
-        description = "Dioxus cross-platform GUI app.";
-      };
-      rust-iot-nrf52 = {
-        path = ./rust-iot-nrf52;
-        description = "Rust IoT on nRF52.";
-      };
-      rust-iot-stm32 = {
-        path = ./rust-iot-stm32;
-        description = "Rust IoT on STM32.";
-      };
-      rust-node = {
-        path = ./rust-node;
-        description = "Rust and NodeJS hybrid project.";
-      };
-      rust-yew = {
-        path = ./rust-yew;
-        description = "Yew Web app.";
-      };
-      tex-basic = {
-        path = ./templates/tex-basic;
-        description = "LaTeX Document.";
-      };
-      vagrant-basic = {
-        path = ./templates/vagrant-basic;
-        description = "Vagrant boilerplate.";
-      };
+    golang-basic = {
+      path = ./golang-basic;
+      description = "Golang project.";
+    };
+    python-basic = {
+      path = ./python-basic;
+      description = "Python project.";
+    };
+    rust-aws-sam = {
+      path = ./rust-aws-sam;
+      description = "Rust in AWS Lambda with SAM.";
+    };
+    rust-basic = {
+      path = ./rust-basic;
+      description = "Rust project.";
+    };
+    rust-dioxus = {
+      path = ./rust-dioxus;
+      description = "Dioxus cross-platform GUI app.";
+    };
+    rust-iot-nrf52 = {
+      path = ./rust-iot-nrf52;
+      description = "Rust IoT on nRF52.";
+    };
+    rust-iot-stm32 = {
+      path = ./rust-iot-stm32;
+      description = "Rust IoT on STM32.";
+    };
+    rust-node = {
+      path = ./rust-node;
+      description = "Rust and NodeJS hybrid project.";
+    };
+    rust-yew = {
+      path = ./rust-yew;
+      description = "Yew Web app.";
+    };
+    tex-basic = {
+      path = ./templates/tex-basic;
+      description = "LaTeX Document.";
+    };
+    vagrant-basic = {
+      path = ./templates/vagrant-basic;
+      description = "Vagrant boilerplate.";
     };
 
   };

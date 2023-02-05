de9eacf96a384088fb31702722c86f7352e4e8e3
Adrian Schmitz <adrian@root.life>
2023-02-05 13:51:37 -0600

    minor tidying

--- rust-google/pmc-google/examples/drive.rs
+++ rust-google/pmc-google/examples/drive.rs
@@ -3,8 +3,14 @@ use pmc_google::drive;
 #[tokio::main]
 async fn main() {
     let drive = drive::GDrive::new().await;
+
+    // Get a list of files whose parents include the folder "Scans"
     drive.list("\"Scans\" in parents").await;
-    // let file_id = "1k9pbDQgUGObsPlue6Zl8LMHJTSagtg6R";
-    let file_id = "1zJugwWbzXx1U5MHB5P8vQyPgAXRrF-4k";
+
+    // Get file id as first cli argument.
+    let file_id = env::args().skip(1).next().unwrap_or_default().to_string();
+    
+    // Fetch the file with the specified ID.
+    // TODO: differentiate "get", "download", and "export". Return File for "get", and bytes for latter two.
     drive.get(file_id).await;
 }
